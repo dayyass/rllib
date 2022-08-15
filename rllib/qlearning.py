f3092ba1df4e48dbee456ae68bef9e0e148d34a3
Dani El-Ayyass <dayyass@yandex.ru>
2022-08-16 01:19:29 +0300

    add dqn agent

--- rllib/qlearning.py
+++ rllib/qlearning.py
@@ -175,6 +175,7 @@ class EVSarsaAgent(QLearningAgent):
         return value
 
 
+# TODO: allow batch processing
 class ApproximateQLearningAgent(_BaseAgent):
     """
     Approximate Q-Learning Agent.
@@ -205,6 +206,19 @@ class ApproximateQLearningAgent(_BaseAgent):
         self.discount = discount
         self.n_actions = n_actions
 
+    def get_qvalues(
+        self,
+        state: np.ndarray,
+    ) -> np.ndarray:
+        """
+        Get Q-values for given state.
+        """
+
+        state = self._to_tensor(state[None])  # TODO: remove [None]
+
+        q_values = self.model(state).detach().cpu().numpy()[0]
+        return q_values
+
     def get_action(
         self,
         state: np.ndarray,
@@ -234,20 +248,18 @@ class ApproximateQLearningAgent(_BaseAgent):
         Compute the best action to take in a state.
         """
 
-        state = self._to_tensor(state[None])
-        q_values = self.model(state).detach().cpu().numpy()[0]
-
+        q_values = self.get_qvalues(state)
         action = int(np.argmax(q_values))
-
         return action
 
+    # TODO: maybe rename
     def update(  # type: ignore
         self,
         states: List[np.ndarray],
         actions: List[int],
         rewards: List[float],
         next_states: List[np.ndarray],
-        is_done: List[bool],
+        is_dones: List[bool],
     ) -> torch.Tensor:  # TODO: maybe do .backward() here
         """
         Compute TD loss:
@@ -258,7 +270,7 @@ class ApproximateQLearningAgent(_BaseAgent):
         actions = self._to_tensor(actions)
         rewards = self._to_tensor(rewards)
         next_states = self._to_tensor(next_states)
-        is_done = self._to_tensor(is_done)
+        is_dones = self._to_tensor(is_dones)
 
         predicted_qvalues = self.model(states)
         predicted_qvalues_for_actions = torch.sum(
@@ -270,7 +282,7 @@ class ApproximateQLearningAgent(_BaseAgent):
         next_state_values = torch.max(predicted_next_qvalues, dim=1)[0]
 
         target_qvalues_for_actions = where(
-            is_done,
+            is_dones,
             rewards,
             rewards + self.discount * next_state_values,
         )
@@ -288,3 +300,131 @@ class ApproximateQLearningAgent(_BaseAgent):
 
         device = next(self.model.parameters()).device
         return torch.tensor(array, dtype=torch.float32, device=device)
+
+
+class DQN(ApproximateQLearningAgent):
+    """
+    Deep Q-Network Agent.
+    """
+
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        target_network: torch.nn.Module,
+        alpha: float,
+        epsilon: float,
+        discount: float,
+        n_actions: int,  # TODO: maybe remove
+    ):
+        """
+        Q-Learning Agent Initialization.
+
+        Args:
+            model (torch.nn.Module): torch neural network.
+            target_network (torch.nn.Module): DQN target network.
+            alpha (float): learning rate.
+            epsilon (float): exploration probability.
+            discount (float): discount rate (aka gamma).
+            n_actions (int): number of possible actions.
+        """
+
+        super().__init__(
+            model=model,
+            alpha=alpha,
+            epsilon=epsilon,
+            discount=discount,
+            n_actions=n_actions,
+        )
+
+        self.target_network = target_network
+
+    def get_qvalues(
+        self,
+        states: np.ndarray,
+    ) -> np.ndarray:
+        """
+        Get Q-values for given states.
+        """
+
+        states = self._to_tensor(states)
+        q_values = self.model(states).detach().cpu().numpy()
+        return q_values
+
+    def get_actions(
+        self,
+        states: np.ndarray,
+    ) -> np.ndarray:
+        """
+        Compute actions in states, including exploration.
+        """
+
+        batch_size = states.shape[0]
+
+        best_actions = self.get_best_actions(states)
+        random_actions = np.random.choice(
+            self.n_actions,
+            size=batch_size,
+        )
+
+        should_explore = np.random.choice(
+            a=[0, 1],
+            size=batch_size,
+            p=[1 - self.epsilon, self.epsilon],
+        )
+
+        actions = np.where(should_explore, random_actions, best_actions)
+        return actions
+
+    def get_best_actions(
+        self,
+        states: np.ndarray,
+    ) -> np.ndarray:
+        """
+        Compute the best actions to take in states.
+        """
+
+        q_values = self.get_qvalues(states)
+
+        actions = np.argmax(q_values, axis=-1)
+        return actions
+
+    # TODO: maybe rename
+    def update(  # type: ignore
+        self,
+        states: np.ndarray,
+        actions: np.ndarray,
+        rewards: np.ndarray,
+        next_states: np.ndarray,
+        is_dones: np.ndarray,
+    ) -> torch.Tensor:  # TODO: maybe do .backward() here
+        """
+        Compute TD loss:
+            L := 1/N * sum {(Q(s,a) - [r(s,a) + gamma * max_over_action[Q(s',a')]])**2}
+        """
+
+        states = self._to_tensor(states)
+        actions = self._to_tensor(actions)
+        rewards = self._to_tensor(rewards)
+        next_states = self._to_tensor(next_states)
+        is_dones = self._to_tensor(is_dones)
+
+        is_not_dones = 1 - is_dones
+
+        predicted_qvalues = self.model(states)
+        predicted_qvalues_for_actions = predicted_qvalues[
+            range(len(actions)), actions
+        ]  # TODO: validate and compare with ApproximateQLearningAgent
+
+        predicted_next_qvalues = self.target_network(next_states)
+        next_state_values = torch.max(predicted_next_qvalues, dim=1)[0]
+
+        # TODO: validate and compare with ApproximateQLearningAgent
+        target_qvalues_for_actions = (
+            rewards + self.discount * next_state_values * is_not_dones
+        )
+
+        loss = torch.mean(
+            (predicted_qvalues_for_actions - target_qvalues_for_actions.detach()) ** 2
+        )
+
+        return loss
